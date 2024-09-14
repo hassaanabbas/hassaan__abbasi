85f338cf9f6ce174ce267e92b3ad9c1dcbde0c2d
hassaanabbas <54630831+hassaanabbas@users.noreply.github.com>
2024-09-14 19:12:31 +0500

    Add files via upload

---
+++ percentage/percentage.js
@@ -0,0 +1,14 @@
+let grade = prompt("Enter Your percentage")
+if(grade > 100)
+    {console.log("You Enter Wrong Percentage");}
+else if(grade >= 80)
+    {console.log("A1");}
+else if(grade >= 70)
+    {console.log("A");}
+else if(grade >= 60)
+    {console.log("B");}
+else if(grade >= 59 || grade >=0 && grade <= 58)
+    {console.log("FAil");}
+else if(grade <0)
+    {console.log("You Enter Wrong Percentage");}
+
